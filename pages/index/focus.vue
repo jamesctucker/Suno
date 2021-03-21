f9858f6e35a677c450e1e9b2c135cb530eace462
James C. Tucker <jamesctucker@protonmail.com>
2021-03-21 18:00:56 -0500

    user will now see their most urgent task on the focus page

--- pages/index/focus.vue
+++ pages/index/focus.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="Focus">
     <c-heading text-align="center">Focus</c-heading>
-    <p v-if="currentToDo">{{ currentToDo.name }}</p>
+    <p v-if="focusToDo">{{ focusToDo.name }}</p>
     <p v-else>you're all caught up! go relax!</p>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import { CHeading } from "@chakra-ui/vue";
 
 export default {
@@ -19,13 +20,7 @@ export default {
     todos: Array
   },
   computed: {
-    currentToDo() {
-      if (this.todos) {
-        return this.todos.filter(todo => todo.complete === false).shift();
-      } else {
-        return undefined;
-      }
-    }
+    ...mapGetters("todos", ["focusToDo"])
   }
 };
 </script>
